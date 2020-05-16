72d21506d521fee06b0ee8246c617dad1c8a7fe1
Brian Minsk <brianminsk@users.noreply.github.com>
2020-05-16 15:38:35 +0700

    added the blog page model and template and started to add tagging

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,14 @@
+from django.db import models
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.search import index
+from modelcluster.fields import ParentalKey
+from modelcluster.contrib.taggit import ClusterTaggableManager
+from taggit.models import TaggedItemBase
+
+class BlogPageTag(TaggedItemBase):
+    content_object = ParentalKey('blog.BlogPage', on_delete=models.CASCADE, related_name='tagged_items')
 
 
 class BlogIndexPage(Page):
@@ -8,4 +16,26 @@ class BlogIndexPage(Page):
 
     content_panels = Page.content_panels + [
         FieldPanel('intro', classname="full")
+    ]
+
+
+class BlogPage(Page):
+    date = models.DateField("Post date")
+    intro = models.CharField(max_length=250)
+    body = RichTextField(blank=True)
+    tags = ClusterTaggableManager(through=BlogPageTag, blank=True)
+
+    search_fields = Page.search_fields + [
+        index.SearchField('intro'),
+        index.SearchField('body'),
+    ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel('date'),
+        FieldPanel('intro'),
+        FieldPanel('body', classname="full"),
+    ]
+
+    promote_panels = Page.promote_panels + [
+        FieldPanel('tags'),
     ]
